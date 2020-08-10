efbfedff9f983be193b40e97985c1537ce86fb18
Smokey <kmboplays@gmail.com>
2020-08-10 13:19:35 +0530

    Wrath here...
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_mido.mk
+    $(LOCAL_DIR)/wrath_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_mido-eng \
-    ancient_mido-userdebug \
-    ancient_mido-user
+    wrath_mido-eng \
+    wrath_mido-userdebug \
+    wrath_mido-user
--- wrath_mido.mk
+++ wrath_mido.mk
@@ -21,28 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common Ancient ROM stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common Wrath stuff.
+$(call inherit-product, vendor/wrath/config/common_full_phone.mk)
 
-# Gapps
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-
-# Bootanimation Resolution
+# Boot Animation Resoltuion
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Some Extra's
-ANCIENT_OFFICIAL=true
+# OFFICIAL Tag
+WRATH_BUILD_TYPE := OFFICIAL
 
-# OTA for Vanilla Variant only
-FORCE_OTA=true
-
-# For Vanilla
-ANCIENT_NOGAPPS=true
+# Maintainer
+WRATH_MAINTAINER := Smokey
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := ancient_mido
+PRODUCT_NAME := wrath_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
